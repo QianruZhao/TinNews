c8fb320cdabefbcddbb434bfb277f8b144756edd
Qianru Zhao <qz@QianrudeMacBook-Pro.local>
2018-09-10 22:21:16 -0400

    First commit.

---
+++ app/src/main/java/com/laioffer/tinnews/MainActivity.java
@@ -0,0 +1,101 @@
+package com.laioffer.tinnews;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.laioffer.tinnews.common.ContainerFragment;
+import com.laioffer.tinnews.common.TinBasicActivity;
+import com.laioffer.tinnews.common.TinBasicFragment;
+import com.laioffer.tinnews.common.TinFragmentPagerAdapter;
+
+public class MainActivity extends TinBasicActivity {
+
+    private ViewPager viewPager;
+    private BottomNavigationView bottomBar;
+    private TinFragmentPagerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //link view pager with view
+        viewPager = findViewById(R.id.viewpager);
+        adapter = new TinFragmentPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        viewPager.setOffscreenPageLimit(TinFragmentPagerAdapter.FRAGMENT_NUMBER);
+
+        //add bottombar code from here
+        bottomBar = findViewById(R.id.bottom_navigation);
+        bottomBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                viewPager.setCurrentItem(ContainerFragment.getPositionById(item.getItemId()));
+                return true;
+            }
+        });
+        //add bottombar code end  here
+
+    }
+
+
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        //add click listener here
+//        findViewById(R.id.text_view).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//    }
+
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_main;
+    }
+
+    private FragmentManager getCurrentChildFragmentManager() {
+        return adapter.getItem(viewPager.getCurrentItem()).getChildFragmentManager();
+    }
+
+
+    @Override
+    public void doFragmentTransaction(TinBasicFragment basicFragment) {
+        FragmentTransaction fragmentTransaction = getCurrentChildFragmentManager().beginTransaction();
+        fragmentTransaction.replace(
+                R.id.child_fragment_container,
+                basicFragment,
+                basicFragment.getFragmentTag()).addToBackStack(null).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        FragmentManager fragmentManager = getCurrentChildFragmentManager();
+        if (fragmentManager.getBackStackEntryCount() > 0) {
+            fragmentManager.popBackStack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @Override
+    public void showSnackBar(String message) {
+
+    }
+}
+
---
+++ app/src/main/java/com/laioffer/tinnews/save/detail/TitleViewModel.java
@@ -0,0 +1,49 @@
+package com.laioffer.tinnews.save.detail;
+
+import android.support.annotation.LayoutRes;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.laioffer.tinnews.R;
+import com.laioffer.tinnews.common.BaseViewModel;
+import com.laioffer.tinnews.common.Util;
+import com.laioffer.tinnews.retrofit.response.News;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class TitleViewModel extends BaseViewModel<TitleViewModel.TitleViewModelHolder> {
+    private String title;
+    public TitleViewModel(String title) {
+        super(R.layout.title_layout);
+        this.title = title;
+    }
+
+    public TitleViewModel(String title, @LayoutRes int layout) {
+        super(layout);
+        this.title = title;
+    }
+
+    @Override
+    public TitleViewModelHolder createItemViewHolder(View view) {
+        return new TitleViewModelHolder(view);
+    }
+
+    @Override
+    public void bindViewHolder(TitleViewModelHolder holder) {
+        holder.title.setText(title);
+    }
+
+
+    static class TitleViewModelHolder extends RecyclerView.ViewHolder {
+        TextView title;
+        TitleViewModelHolder(View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.title);
+        }
+    }
+
+
+
+}
